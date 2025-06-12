ddb4ebd1131a5c8beac13f550d51db94e9e7084c
yeessonng <dorlek856@gmail.com>
2025-06-12 23:57:04 +0900

    feat: 로그인 토큰 자동화 추가

--- accounts/utils.py
+++ accounts/utils.py
@@ -4,9 +4,9 @@ from signin.models import User
 
 def get_user_from_access_token(token):
     try:
-        access = AccessToken(token)
-        user_id = access['user_id']  # payload에서 추출
+        access = AccessToken(token) #유효하냐?
+        user_id = access['user_id']  #payload에서 추출 - user pk
         user = User.objects.get(id=user_id)
-        return user
+        return user #토큰 user 추출
     except Exception:
         raise AuthenticationFailed("유효하지 않은 토큰입니다.")
