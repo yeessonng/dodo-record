0653d7366a3fce2f05e63f1117c9b4471a62ee30
yujining3827 <j_yujin_swu22@naver.com>
2025-06-13 17:47:12 +0900

    fix : 지역명으로 반환

--- travelList/views.py
+++ travelList/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from travelPost.models import Region, Post
 
 def index(request):
     return render(request, 'travelList/travelList.html')
 
 def region_post_list(request, pk):
-    return render(request, 'travelList/travelList.html', {'region_id': pk})
+    region = get_object_or_404(Region, pk=pk)  # region 변수 선언
+    return render(request, 'travelList/travelList.html', {'region_name': region.region})
+
+def detail(request):
+    if not request.user.is_authenticated:
+        return redirect('/login/')
+
+    region = request.GET.get('region')
+    print("region:", region)
+
+    return render(request, 'travelPostTemp/travelPostTemp.html')
